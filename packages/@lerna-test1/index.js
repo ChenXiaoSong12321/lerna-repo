529c96f1e406aeb63de70dd6e0cd8f83766d89dd
Jerry12138 <448216252@qq.com>
2019-08-12 18:07:26 +0800

    sdsd

--- packages/@lerna-test1/index.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  a = 3,
-  b = 666,
-  hh = 'sddfsfes',
-  log(){
-    console.log('module1');
-  }
-}
---
+++ packages/lerna-test1/index.js
@@ -0,0 +1,8 @@
+module.exports = {
+  a = 3,
+  b = 6366,
+  hh = 'sddfsfes',
+  log(){
+    console.log('module1');
+  }
+}
